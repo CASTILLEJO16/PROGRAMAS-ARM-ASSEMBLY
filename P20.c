b761f94544cfdf7d6e68f3160043e6326042fb89
LENNYN ALEJANDRO CASTILLEJO ROBLES <144736810+CASTILLEJO16@users.noreply.github.com>
2024-11-07 16:35:15 -0800

    Create P20.c

---
+++ P20.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Declaramos la función de ensamblador
+extern void matrix_multiplication();
+
+// Declaramos las matrices como externas
+extern int matrizA[3][3];
+extern int matrizB[3][3];
+extern int resultado[3][3];
+
+int main() {
+    // Llamamos a la función de ensamblador
+    matrix_multiplication();
+
+    // Imprimimos el resultado
+    printf("Resultado de la multiplicación de matrices:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", resultado[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
