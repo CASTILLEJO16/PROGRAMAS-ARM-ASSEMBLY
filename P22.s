57f53be3d55001e160d02933bee96ecb3e78e994
LENNYN ALEJANDRO CASTILLEJO ROBLES <144736810+CASTILLEJO16@users.noreply.github.com>
2024-11-11 16:43:45 -0800

    Create P22.s

---
+++ P22.s
@@ -0,0 +1,21 @@
+/*╔════════════════════════════════════════════════════════════════════╗
+║                    TECNOLÓGICO NACIONAL DE MÉXICO                 ║
+║                     INSTITUTO TECNOLÓGICO DE TIJUANA              ║
+╠════════════════════════════════════════════════════════════════════╣
+║                   Carrera: Ingeniería en Sistemas                 ║
+║                   Nombre: Lennyn Alejandro Castillejo Robles      ║
+║                   Matrícula: 22210880                             ║
+║                   Materia: INTERFAZ DE USUARIO                                         ║
+║                                                                                                                  ║
+╠════════════════════════════════════════════════════════════════════╣
+║                           Septiembre 2024                         ║
+╚════════════════════════════════════════════════════════════════════╝
+*/
+.section .text
+.global ascii_to_int
+
+ascii_to_int:
+    // La entrada es el carácter en el registro w0
+    // Resta el valor ASCII de '0' (48) para obtener el valor entero
+    sub w0, w0, #'0'
+    ret
