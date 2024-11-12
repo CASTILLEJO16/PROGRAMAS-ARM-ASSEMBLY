107ec146546fb7452a94934ae497b504d69ea4a2
LENNYN ALEJANDRO CASTILLEJO ROBLES <144736810+CASTILLEJO16@users.noreply.github.com>
2024-11-11 16:44:48 -0800

    Create P23.s

---
+++ P23.s
@@ -0,0 +1,22 @@
+/*╔════════════════════════════════════════════════════════════════════╗
+║                    TECNOLÓGICO NACIONAL DE MÉXICO                 ║
+║                     INSTITUTO TECNOLÓGICO DE TIJUANA              ║
+╠════════════════════════════════════════════════════════════════════╣
+║                   Carrera: Ingeniería en Sistemas                 ║
+║                   Nombre: Lennyn Alejandro Castillejo Robles      ║
+║                   Matrícula: 22210880                             ║
+║                   Materia: INTERFAZ DE USUARIO                                         ║
+║                                                                                                                  ║
+╠════════════════════════════════════════════════════════════════════╣
+║                           Septiembre 2024                         ║
+╚════════════════════════════════════════════════════════════════════╝
+*/
+// Convierte un número entero (0-9) en un carácter ASCII correspondiente
+.section .text
+.global int_to_ascii
+
+int_to_ascii:
+    // La entrada es el número en el registro w0
+    // Sumar el valor ASCII de '0' (48) para obtener el carácter ASCII
+    add w0, w0, #'0'
+    ret
