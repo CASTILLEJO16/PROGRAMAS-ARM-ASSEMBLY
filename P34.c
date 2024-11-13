39f98d72f06b1bf70aef481ad17f0c6358e0af87
LENNYN ALEJANDRO CASTILLEJO ROBLES <144736810+CASTILLEJO16@users.noreply.github.com>
2024-11-13 11:10:00 -0800

    Update P34.c

--- P34.c
+++ P34.c
@@ -10,6 +10,8 @@
 ╠════════════════════════════════════════════════════════════════════╣
 ║                           Septiembre 2024                         ║
 ╚════════════════════════════════════════════════════════════════════*/
+//ASCIINEMA: https://asciinema.org/a/ha5WAgezGJ7A5yUZoJkFrdOyv
+
 #include <stdio.h>
 
 // Declaración de la función en ensamblador
