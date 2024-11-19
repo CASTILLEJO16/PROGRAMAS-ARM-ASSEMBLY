8c5cb13410c6ada860503fd5146b59dfb90aead9
LENNYN ALEJANDRO CASTILLEJO ROBLES <144736810+CASTILLEJO16@users.noreply.github.com>
2024-11-18 19:35:07 -0800

    Update P5.s

--- P5.s
+++ P5.s
@@ -8,7 +8,7 @@
 // ║                   Materia: Programación en Ensamblador            ║
 // ║                                                                   ║
 // ╠════════════════════════════════════════════════════════════════════╣
-// ║                           Octubre 2024                            ║
+// ║                          noviembre 2024                            ║
 // ╚════════════════════════════════════════════════════════════════════╝
 //ASCIINEMA:https://asciinema.org/a/KmP25EZV8IphEyhLuc2oj8sGm
 // Código en C# (comentado):
