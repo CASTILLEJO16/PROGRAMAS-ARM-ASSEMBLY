be3057432bda5c26b419486e2e48b0d64baf65cc
LENNYN ALEJANDRO CASTILLEJO ROBLES <144736810+CASTILLEJO16@users.noreply.github.com>
2024-11-18 19:31:56 -0800

    Update P8.s

--- P8.s
+++ P8.s
@@ -10,7 +10,7 @@
 // ╠════════════════════════════════════════════════════════════════════╣
 // ║                           Septiembre 2024                         ║
 // ╚════════════════════════════════════════════════════════════════════╝
-
+//ASCIINEMA:https://asciinema.org/a/KmP25EZV8IphEyhLuc2oj8sGm
 // Código en C# para calcular y mostrar la serie de Fibonacci hasta N términos ingresados por el usuario.
 /*
 using System;
